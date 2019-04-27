c9b2b007065a2cd6ccd409cfc2de0ff0ef000225
gonzalo16 <marinucci.gonzalo@gmail.com>
2019-04-28 00:34:17 +0200

    Crear el sistema de logeo de admin y profesor

--- app/src/main/java/com/example/usuario/asistenciaprofesorv1/modelo/VentanaAdmin.java
+++ app/src/main/java/com/example/usuario/asistenciaprofesorv1/modelo/VentanaAdmin.java
@@ -1,15 +1,24 @@
 package com.example.usuario.asistenciaprofesorv1.modelo;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.usuario.asistenciaprofesorv1.R;
 
 public class VentanaAdmin extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ventana_admin);
+
+        toolbar=findViewById(R.id.toolbar);
+        toolbar.setTitle("Administracion");
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
     }
 }
